dee323892e4159dc40321304ed6d4016a1bb1679
bder222 <57365331+bder222@users.noreply.github.com>
2021-01-25 18:58:31 -0500

    Update NBA_Render.py

--- NBA_Render.py
+++ NBA_Render.py
@@ -7,20 +7,20 @@ import sys
 
 class Render:
     def __init__(self):
-        self.options = RGBMatrixOptions()
-        self.options.hardware_mapping = 'adafruit-hat'
-        self.options.gpio_slowdown = 3
+        #self.options = RGBMatrixOptions()
+        #self.options.hardware_mapping = 'adafruit-hat'
+        #self.options.gpio_slowdown = 3
         self.options.rows = 32
         self.options.cols = 64
         self.options.drop_privileges = False
         
         
-        self.path = '/home/pi/NBA-led-Scoreboard/'
+        self.path = '/home/pi/NBA-scoreboard/'
         
         self.font = graphics.Font()
-        self.font.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/6x12.bdf")
+        self.font.LoadFont("/home/pi/NBA-scoreboard/rpi-rgb-led-matrix/fonts/6x12.bdf")
         self.font2 = graphics.Font()
-        self.font2.LoadFont("/home/pi/rpi-rgb-led-matrix/fonts/4x6.bdf")
+        self.font2.LoadFont("/home/pi/NBA-scoreboard/rpi-rgb-led-matrix/fonts/4x6.bdf")
         self.team_colors = {'ATL': [[225, 58, 62], [100, 100, 100]], 'BOS': [[0, 131, 72], [187, 151, 83]], 'BKN': [[100, 100, 100], [0, 0, 0]], 'CHA': [[29, 17, 96], [0, 140, 168]], 'CHI': [[206, 17, 65], [0, 0, 0]], 'CLE': [[134, 0, 56], [253, 187, 48]], 'DAL': [[0, 125, 197], [196, 206, 211]], 'DEN': [[77, 144, 205], [253, 185, 39]], 'DET': [[237, 23, 76], [0, 107, 182]], 'GSW': [[253, 185, 39], [0, 107, 182]], 'HOU': [[206, 17, 65], [196, 206, 211]], 'LAL': [[253, 185, 39], [85, 37, 130]], 'MEM': [[15, 88, 108], [190, 212, 233]], 'MIA': [[152, 0, 46], [0, 0, 0]], 'MIL': [[0, 71, 27], [240, 235, 210]], 'MIN': [[0, 80, 131], [0, 169, 79]], 'NOP': [[0, 43, 92], [227, 24, 55]], 'NYK': [[0, 107, 182], [245, 132, 38]], 'OKC': [[0, 125, 195], [240, 81, 51]], 'ORL': [[0, 125, 197], [0, 0, 0]], 'PHI': [[237, 23, 76], [0, 107, 182]], 'PHX': [[229, 96, 32], [29, 17, 96]], 'POR': [[224, 58, 62], [186, 195, 201]], 'SAC': [[114, 76, 159], [142, 144, 144]], 'SAS': [[186, 195, 201], [0, 0, 0]], 'TOR': [[206, 17, 65], [0, 0, 0]], 'UTA': [[0, 43, 92], [249, 160, 27]], 'WAS': [[0, 43, 92], [227, 24, 55]], 'IND': [[255, 198, 51], [0, 39, 93]], 'LAC': [[237, 23, 76], [0, 107, 182]]}
             
     def Render_Games(self, printer=False):
